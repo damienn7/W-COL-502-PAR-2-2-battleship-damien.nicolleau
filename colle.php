a1e0e6f048a9300a293e5daa3f14d3753da5081e
damienn7 <damien.nicolleau@epitech.eu>
2023-05-31 14:40:09 +0200

    ADD - step 2 tested and validated

--- colle.php
+++ colle.php
@@ -1,6 +1,6 @@
 <?php
 
-colle(3,2);
+colle(5,3,[[0, 0], [3, 2]]);
 
 function colle($x,$y,array $coords = []){
     display($x,$y,$coords);
@@ -13,7 +13,7 @@ function display($x,$y,$coords){
     for ($i=0; $i <= $y; $i++) { 
         $table .= $border;
         for ($j=0; $j <= $x; $j++) { 
-            $table .= "| ".isEmpty($x,$y,$coords)." "; 
+            $table .= "| ".isEmpty($j,$i,$coords)." "; 
         }
         $table .= "|\n";
     }
@@ -22,7 +22,12 @@ function display($x,$y,$coords){
 }
 
 function isEmpty($x,$y,$coords){
-    
+    foreach ($coords as $key => $value) {
+        if ($value[0]==$x&&$value[1]==$y) {
+            return "X";
+        }
+    }
+
     return " ";
 }
 function prompt($x,$y,$coords){
